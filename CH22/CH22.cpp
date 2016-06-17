9fe0e94a220271a4aeeee09df291db3024da3248
jpbra <jpbrastad@gmail.com>
2016-06-16 17:53:49 -0700

    Added for 22.2

--- CH22/CH22.cpp
+++ CH22/CH22.cpp
@@ -40,6 +40,26 @@ ElementType minElement(ContainerType& container)
 	return temp;
 }
 
+/*
+22.2 Find position of a value in first-class container, -1 if value not found.
+*/
+
+template<typename ElementType, typename ContainerType>
+int find(ContainerType& container, const ElementType& value)
+{
+	int counter = 0;
+	for (auto p = container.begin(); p != container.end(); p++)
+	{
+		if (*p == value)
+		{
+			return counter;
+		}
+		counter++;
+	}
+
+	return -1;
+}
+
 int main()
 {
 	vector<int> intVector;
@@ -51,6 +71,8 @@ int main()
 
 	int iResultMax = maxElement<int>(intVector);
 	int iResultMin = minElement<int>(intVector);
+	int positionTrue = find(intVector, 30);
+	int positionFalse = find(intVector, 8);
 
 	list<double> sList;
 	sList.push_back(1.1);
@@ -60,6 +82,8 @@ int main()
 
 	double sResultMax = maxElement<double>(sList);
 	double sResultMin = minElement<double>(sList);
+	int posTrue = find(sList, 3.9);
+	int posFalse = find(sList, 3.14);
 
     return 0;
 }
